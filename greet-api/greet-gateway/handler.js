d385d95e69664527d7067d39c24048cf1a109fa9
James <wilfongjt@gmail.com>
2019-07-30 11:28:19 -0400

    greet-serverless

--- greet-api/greet-gateway/handler.js
+++ greet-api/greet-gateway/handler.js
@@ -1,10 +1,26 @@
 'use strict';
 const AWS = require('aws-sdk');
 module.exports.hello = async (event) => {
+  let message = "hello";
+  const name = event.queryStringParameters && event.queryStringParameters.name;
+  if (name !== null) {
+    message = "hello " + name;
+  }
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless dev v1.0! Your function executed successfully!',
+      message: message,
+      input: event,
+    }, null, 2),
+  };
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+module.exports.goodbye = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Goodbye function executed successfully!',
       input: event,
     }, null, 2),
   };
